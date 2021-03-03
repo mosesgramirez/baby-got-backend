4b70c85be4bef853136644aefa2a79cfc5e2717e
Moses Ramirez <mosesgramirez@gmail.com>
2021-03-03 17:31:59 -0600

    Node callbacks and error handling

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,13 @@
-// "exports" is shorthand for "module.exports"
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+// "exports" is shorthand for "module.exports", can use if you are only using a particular method of an export
+// Node error callbacks - convention is for aync funtions to return an error object as the first param
+module.exports = (x, y, cbFunc) => {
+    if (x <= 0 || y <= 0) {
+        cbFunc(new Error(`Rectangle dimensions ${x}, ${y} must be less than zero.`));
+    } else {
+        setTimeout(() => cbFunc(null, {
+            perimeter: () => 2 * (x + y),
+            area: () => x * y
+        }), 2000);
+    }
+}
+
