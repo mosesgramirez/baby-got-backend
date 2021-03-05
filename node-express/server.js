ca2cbcff1ed8bfdc0f08142ed26b4849aaa4d9d1
Moses Ramirez <mosesgramirez@gmail.com>
2021-03-05 17:35:17 -0600

    Add Morgan and Express.static

--- node-express/server.js
+++ node-express/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
 // This is considered a middleware function in express. 'next' is an option third param which is a function instead of an object.
 app.use((req, res) => {
-    console.log(req.headers);
+    // console.log(req.headers); [morgan will now handle this]
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express server</html></body></h1>');
