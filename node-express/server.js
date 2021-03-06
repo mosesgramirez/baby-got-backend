315c285a363b25d56577cfd598f3a546eff75135
Moses Ramirez <mosesgramirez@gmail.com>
2021-03-06 08:10:43 -0600

    Set up REST API endpoints using Express routing methods

--- node-express/server.js
+++ node-express/server.js
@@ -7,7 +7,49 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next(); // statusCode and setHeader are passed by the next() menthod to all subsequent relevant router endpoints.
+});
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite "${req.body.name}" with description "${req.body.description}"`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403; // forbidden
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite "${req.params.campsiteId}" to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403; 
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite ${req.params.campsiteId}\n`)
+    res.end(`Will update the campsite "${req.body.name}" with the description "${req.body.description}"`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting the campsite "${req.params.campsiteId}"`);
+});
 
 // This is considered a middleware function in express. 'next' is an option third param which is a function instead of an object.
 app.use((req, res) => {
